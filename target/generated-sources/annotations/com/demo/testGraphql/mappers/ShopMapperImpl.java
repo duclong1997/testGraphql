d1c229dd741e27f59b3f74973af1c6e5ebc2a39f
long nguyen <longnd.mangoteck@gmail.com>
2021-04-14 11:34:30 +0700

    udpate

--- target/generated-sources/annotations/com/demo/testGraphql/mappers/ShopMapperImpl.java
+++ target/generated-sources/annotations/com/demo/testGraphql/mappers/ShopMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-14T11:21:24+0700",
+    date = "2021-04-14T11:33:57+0700",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
