45700f0510673327b3c1c67ef71428ead26ea981
long nguyen <longnd.mangoteck@gmail.com>
2021-04-14 10:23:36 +0700

    update

--- target/generated-sources/annotations/com/demo/testGraphql/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/demo/testGraphql/mappers/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-14T02:17:07+0700",
+    date = "2021-04-14T10:20:36+0700",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
