66343812f337e7521ecf7099a4bb64f789c48247
long nguyen <longnd.mangoteck@gmail.com>
2021-04-15 22:53:02 +0700

    uda

--- src/main/java/com/demo/testGraphql/resolvers/user/UserMutationResolver.java
+++ src/main/java/com/demo/testGraphql/resolvers/user/UserMutationResolver.java
@@ -3,14 +3,18 @@ package com.demo.testGraphql.resolvers.user;
 import com.demo.testGraphql.models.dtos.RegisterUser;
 import com.demo.testGraphql.models.dtos.UserDto;
 import com.demo.testGraphql.services.UserService;
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
 import graphql.kickstart.tools.GraphQLMutationResolver;
+import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserMutationResolver implements GraphQLMutationResolver {
 
     @Autowired
@@ -23,7 +27,18 @@ public class UserMutationResolver implements GraphQLMutationResolver {
     }
 
     @PreAuthorize("isAnonymous()")
-    public UserDto register(RegisterUser userRegister){
+    public UserDto register(RegisterUser userRegister) {
         return this.userService.register(userRegister);
+    }
+
+    // sử dụng với form-data
+    // upload file
+    public UserDto uploadFileImg(DataFetchingEnvironment environment) {
+        DefaultGraphQLServletContext context = environment.getContext();
+
+        context.getFileParts().forEach(part -> {
+            log.info("upload file: {}, size {}", part.getSubmittedFileName(), part.getSize());
+        });
+        return new UserDto();
     }
 }
