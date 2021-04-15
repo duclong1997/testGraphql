85532ab86bdca64d5cce026c3365e0a0162dfb9a
long nguyen <longnd.mangoteck@gmail.com>
2021-04-15 15:17:19 +0700

    update

---
+++ src/main/java/com/demo/testGraphql/resolvers/role/RoleMutationResolver.java
@@ -0,0 +1,10 @@
+package com.demo.testGraphql.resolvers.role;
+
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RoleMutationResolver implements GraphQLMutationResolver {
+}
---
+++ src/main/java/com/demo/testGraphql/resolvers/role/RoleQueryResolver.java
@@ -0,0 +1,10 @@
+package com.demo.testGraphql.resolvers.role;
+
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RoleQueryResolver implements GraphQLQueryResolver {
+}
